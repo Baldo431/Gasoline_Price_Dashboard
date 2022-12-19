303ab3ec6af00e26114427e9c82ed94055ce2f53
Annette Donald Blackburn <109121304+annette-blackburn@users.noreply.github.com>
2022-12-19 14:44:42 -0700

    Add files via upload
    - Added diesel price prediction linear regression model with visualizations  (scatters of testing and training, graph of R2)
    - Added current crude oil prices to linear regression models (diesel and gas)
    - Added dictionary formats for website (both LRML model and NLP model)
    - Reformatted Twitter data to include date

--- gas_oil_LR_twitter_NLP.py
+++ gas_oil_LR_twitter_NLP.py
@@ -1,3 +1,5 @@
+# see gas_oil_requirements.txt 
+
 # Gas & Oil Linear Regression Model
 import os
 import numpy as np
@@ -36,10 +38,10 @@ hist_gas_df = pd.read_csv("PET_PRI_GND_DCUS_NUS_W.csv")
 hist_gas_df['date'] = hist_gas_df['Date']
 hist_gas_df = hist_gas_df[['date', 'R1', 'D1']]
 
-crude_df = pd.read_csv("crude-oil-price.csv")
+hist_crude_df = pd.read_csv("crude-oil-price.csv")
 
 # Dropping percent change and change of crude oil prices
-crude_df = crude_df[['date', 'price']]
+hist_crude_df = hist_crude_df[['date', 'price']]
 
 # Formatting crude_df to match hist_gas_df date format
 from datetime import date
@@ -49,11 +51,11 @@ def fix_date(bad_date):
     day = day[0:2]
     return date(int(year), int(month), int(day))
 
-crude_df['date'] = crude_df['date'].apply(fix_date) 
+hist_crude_df['date'] = hist_crude_df['date'].apply(fix_date) 
 
-# Dropping 1983-1994 from crude_df
+# Dropping 1983-1994 from hist_crude_df
 cuttoff_date = date(1994, 12, 31)
-crude_df = crude_df.loc[crude_df['date'] > cuttoff_date]
+hist_crude_df = hist_crude_df.loc[hist_crude_df['date'] > cuttoff_date]
 
 # Lining up months and years in crude_df and gas_df
 def date2str(dt):
@@ -63,11 +65,41 @@ def drop_month(dt):
     month, day, year = dt.split('/')
     return f"{month}/{year}"
 
-crude_df['date'] = crude_df['date'].apply(date2str)
-hist_gas_df['date'] = gas_df['date'].apply(drop_month)
+hist_crude_df['date'] = hist_crude_df['date'].apply(date2str)
+hist_gas_df['date'] = hist_gas_df['date'].apply(drop_month)
 
 # Omit 2022 data 
-crude_df = crude_df.loc[0:454]
+hist_crude_df = hist_crude_df.loc[0:454]
+
+# Current Crude Oil Price
+current_crude_df = pd.read_csv("current-oil-prices.csv")
+current_crude_df = current_crude_df[['Date', 'Crude Closing']]
+
+# Rename columns so current_crude_df and hist_crude_df line up
+current_crude_df = current_crude_df.rename(columns={'Date': 'date', 'Crude Closing': 'price'})
+def ref_date(date):
+    date = str(date)
+    y, m, d = date.split('-')
+    return f"{m}/{y}"
+current_crude_df['date'] = current_crude_df['date'].apply(ref_date)
+current_crude_df.head()
+
+# Keep only first Month/Year instance for current_crude_df
+seen_dates = set()
+indices_to_remove = []
+for idx, row in current_crude_df.iterrows():
+    _date = row['Date']
+    if _date in seen_dates:
+        indices_to_remove.append(idx)
+    else:
+        seen_dates.add(_date)
+current_crude_df.drop(indices_to_remove, inplace=True)
+
+# Appending hist_crude_df and current_crude_df
+crude_df = pd.concat([hist_crude_df, current_crude_df], axis=0)
+
+# Rename columns so current_crude_df and hist_crude_df line up
+current_crude_df = current_crude_df.rename(columns={'Date': 'date', 'Crude Closing': 'price'})
 
 # Keep only first Month/Year instance for gas_df
 seen_dates = set()
@@ -82,7 +114,6 @@ hist_gas_df.drop(indices_to_remove, inplace=True)
 
 # Current Gas Prices DF Data Cleaning
 current_gas_df = pd.read_csv("gas-diesel-prices.csv")
-
 
 # Lining up months and years in current_gas_df to match crude_df and hist_gas_df
 def date2str(dt):
@@ -109,6 +140,8 @@ current_gas_df = current_gas_df[['date', 'Regular', 'Diesel']]
 # Appending hist_gas_df and current_gas_df
 gas_df = hist_gas_df.append(current_gas_df)
 
+# Appending hist_crude_df and current_crude_df
+crude_df = pd.concat([hist_crude_df, current_crude_df], axis=0)
 
 # Linear Regression Machine Learning Model
 
@@ -118,12 +151,14 @@ gas_df = hist_gas_df.append(current_gas_df)
 # Explanation of Model Choice, including Limitations and Benefits 
 # This linear regression model is the best model because historically, crude oil prices and gas prices have a linear relationship, so there is no need to complicate the relationship with other, more complex models.
 
+# Gas Linear Reg Model 
 # X = crude oil price [=] 1/barrel
 # Y = gas price [=] 1/gallon
 import numpy as np
 
 X = np.array(crude_df['price'])
 Y = gas_df['Regular'].to_numpy()
+f"X.shape = {X.shape}, Y.shape = {Y.shape}"
 
 ## Train, Test, Split Description 
 # The data are split into 80% training and 20% testing.
@@ -134,19 +169,19 @@ X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_
 X_train = X_train.reshape((-1,1))
 X_test = X_test.reshape((-1,1))
 
-lin_reg_model = LinearRegression()
-lin_reg_model.fit(X_train, y_train)
+gas_lin_reg_model = LinearRegression()
+gas_lin_reg_model.fit(X_train, Y_train)
 
 # R-squared (how much of the outcome is predicted correctly by the model) with training data
-r_2_train = lin_reg_model.score(X_train, y_train)
+r_2_train = gas_lin_reg_model.score(X_train, Y_train)
 r_2_train
 
 from matplotlib import pyplot as plt
 
-predictions = lin_reg_model.predict(X_test)
-r_2_test = lin_reg_model.score(X_test, y_test)
-diffs = [0]*len(y_test)
-for (pred, real, idx) in zip(predictions, y_test, range(len(y_test))):
+predictions = gas_lin_reg_model.predict(X_test)
+r_2_test = gas_lin_reg_model.score(X_test, Y_test)
+diffs = [0]*len(Y_test)
+for (pred, real, idx) in zip(predictions, Y_test, range(len(Y_test))):
     diffs[idx] = pred - real
 
 # Plot of difference of real data from best fit line 
@@ -157,23 +192,99 @@ fig.show()
 # R-squared (how much of the outcome is predicted correctly by the model) with test data
 r_2_test
 
-# Visualizations
+# Visualizations for Gas Lin Reg Model 
 # Training Data Vis
-plt.scatter(X_train, Y_train, color = "mediumvioletred")
-plt.plot(X_train, lin_reg_model.predict(X_train), color = "cornflowerblue")
+plt.scatter(X_train, Y_train, color = "forestgreen")
+plt.plot(X_train, gas_lin_reg_model.predict(X_train), color = "coral")
 plt.title("Model Prediction from Training Data")
 plt.xlabel("Historical Crude Oil (USD/Barrel)")
 plt.ylabel("Model Predicted Regular Gas (USD/Gallon)")
 plt.text(20, 4.0, f"r^2 = {r_2_train:.3f}")
-plt.savefig("training_fig.png")
+plt.savefig("gas_training_fig.png")
 # Test Data Vis
 plt.scatter(X_test, Y_test, color = "forestgreen")
-plt.plot(X_test, lin_reg_model.predict(X_test), color = "coral")
+plt.plot(X_test, gas_lin_reg_model.predict(X_test), color = "coral")
 plt.title("Model Prediction from Testing Data")
 plt.xlabel("Historical Crude Oil (USD/Barrel)")
 plt.ylabel("Model Predicted Regular Gas (USD/Gallon)")
 plt.text(22, 3.2, f"r^2 = {r_2_test:.3f}")
-plt.savefig("testing_fig.png")
+plt.savefig("gas_testing_fig.png")
+
+# Diesel Lin Reg Model
+# X = crude oil price [=] 1/barrel
+# Y = gas price [=] 1/gallon
+import numpy as np
+
+X = np.array(crude_df['price'])
+Y = gas_df['Diesel'].to_numpy()
+f"X.shape = {X.shape}, Y.shape = {Y.shape}"
+
+#### Train, Test, Split Description 
+# The data are split into 80% training and 20% testing.
+
+# Train Test Split
+X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size=0.2, random_state=80)
+
+X_train = X_train.reshape((-1,1))
+X_test = X_test.reshape((-1,1))
+
+X_train.shape
+
+diesel_lin_reg_model = LinearRegression()
+diesel_lin_reg_model.fit(X_train, Y_train)
+
+# R-squared (how much of the outcome is predicted correctly by the model) with training data
+r_2_train = diesel_lin_reg_model.score(X_train, Y_train)
+r_2_train
+
+from matplotlib import pyplot as plt
+
+predictions = diesel_lin_reg_model.predict(X_test)
+r_2_test = diesel_lin_reg_model.score(X_test, Y_test)
+diffs = [0]*len(Y_test)
+for (pred, real, idx) in zip(predictions, Y_test, range(len(Y_test))):
+    diffs[idx] = pred - real
+
+# Plot of difference of real data from best fit line 
+fig, ax = plt.subplots()
+ax.plot(diffs)
+fig.show()
+
+# R-squared (how much of the outcome is predicted correctly by the model) with test data
+r_2_test
+
+# Visualizations for Diesel Lin Reg Model
+# Training Data Vis
+plt.scatter(X_train, Y_train, color = "mediumvioletred")
+plt.plot(X_train, diesel_lin_reg_model.predict(X_train), color = "cornflowerblue")
+plt.title("Model Prediction from Training Data")
+plt.xlabel("Historical Crude Oil (USD/Barrel)")
+plt.ylabel("Model Predicted Diesel (USD/Gallon)")
+plt.text(20, 4.0, f"r^2 = {r_2_train:.3f}")
+plt.savefig("diesel_training_fig.png")
+# Test Data Vis
+plt.scatter(X_test, Y_test, color = "mediumvioletred")
+plt.plot(X_test, diesel_lin_reg_model.predict(X_test), color = "cornflowerblue")
+plt.title("Model Prediction from Testing Data")
+plt.xlabel("Historical Crude Oil (USD/Barrel)")
+plt.ylabel("Model Predicted Diesel (USD/Gallon)")
+plt.text(22, 3.2, f"r^2 = {r_2_test:.3f}")
+plt.savefig("diesel_testing_fig.png")
+
+# Dictionary Format for Website
+# Function that takes in Current Crude Oil Price and returns predicted Gas Price in dictionary format
+def predict_price(crude_price):
+    crude_price = np.array([crude_price])
+    crude_price = np.expand_dims(crude_price, axis=1)
+    gas_prediction = gas_lin_reg_model.predict(crude_price)
+    diesel_prediction = diesel_lin_reg_model.predict(crude_price)
+    return {"Current Closing Price": crude_price[0][0], "Predicted Regular Gas Price": gas_prediction[0], "Predicted Diesel Price": diesel_prediction[0]}
+
+# Example of 12/19/22 crude oil price ($75.65)
+predict_price(75.65)
+
+# Results: {'Current Closing Price': 75.65, 'Predicted Regular Gas Price': 2.9166473070570174,'Predicted Diesel Price': 3.2435708724468726}
+# 12/19/22 current national average regular gas price ($3.142) and diesel price ($4.75)
 
 
 # Twitter Sentiment Analysis (Natural Language Processing) Model 
@@ -194,13 +305,19 @@ os.listdir(os.curdir)
 ### Description of Preliminary Feature Engineering and Preliminary Feature Selection
 #To use natural language processing to characterize the sentiment of current tweets on gas prices (positive, neutral, or negative sentiment, ranging from -1.0 to 1.0).
 
-twitter_df = pd.read_csv('tweets_clean.csv')
+twitter_mess = pd.read_csv("tweets_clean.csv")
+twitter_mess.head()
 
-switchbag = twitter_df.values.reshape(43043, 1)
-twitter_df = pd.DataFrame(switchbag)
+twitter_dict = {'date': [], 'tweet': []}
+for column_name, column_content in twitter_mess.items():
+    if column_name=="Unnamed: 0": continue
+    current_date = str(column_name)
+    for current_tweet in column_content: 
+        twitter_dict['date'].append(current_date)
+        twitter_dict['tweet'].append(current_tweet)
 
-# Adding column name to twitter_df
-twitter_df.columns = ['Tweet', 'sentiment']
+twitter_df = pd.DataFrame(twitter_dict)
+twitter_df
 
 # Example with actual tweet
 example = TextBlob("rtr: U.S. seeks to limit flaring and methane leaks from public lands drilling - President Joe Biden's administration on Monday proposed rules aimed at limiting methane leaks from oil and gas drilling on public lands. By @ValerieVolco @nicholagroom https://t.co/Pi6vCf6RyC")
@@ -208,6 +325,13 @@ example = TextBlob("rtr: U.S. seeks to limit flaring and methane leaks from publ
 for Tweet in twitter_df.columns:
     a = TextBlob(Tweet)
     twitter_df['sentiment'] = a.sentiment.polarity
+
+sentiments = []
+for Tweet in twitter_df['tweet'].to_list():
+    a = TextBlob(Tweet)
+    sentiments.append(a.sentiment.polarity)
+
+twitter_df['sentiment'] = sentiments
 
 # Returning sentiment score of inputted tweet 
 def get_sentimental(target):
@@ -220,8 +344,11 @@ def any2str(target):
     else:
         return target
 
-twitter_df['Tweet'] = twitter_df['Tweet'].apply(any2str)
-twitter_df['sentiment'] = twitter_df['Tweet'].apply(get_sentimental)
+twitter_df['tweet'] = twitter_df['tweet'].apply(any2str)
+twitter_df['sentiment'] = twitter_df['tweet'].apply(get_sentimental)
+
+# Dictionary Format for Website 
+twitter_dict = twitter_df.to_dict()
 
 # Visualizations 
 import matplotlib.pyplot as plt
